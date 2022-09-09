3615b31e58d806edc428d8238471810bb51a3545
lvsouza <ls18017@gmail.com>
2022-09-09 11:49:16 -0300

    feat: some improvements

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+
+import { vuetify } from './plugins/vuetify'
+import { router } from './router'
 import './registerServiceWorker'
-import router from './router'
-import vuetify from './plugins/vuetify'
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -9,21 +9,11 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'home',
     component: HomePageView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutPageView.vue')
   }
 ]
 
-const router = new VueRouter({
+export const router = new VueRouter({
+  routes,
   mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+  base: process.env.BASE_URL
 })
-
-export default router
